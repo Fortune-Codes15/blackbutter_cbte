efdd1f6c996d3cf443f757911c49d81e1d3ca49e
Fortune <fortunefoluso26@gmail.com>
2025-07-06 22:39:50 +0100

    Fixed up folder structure

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var app = express();
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+  res.render('home');
+});
+
 app.use(function(req, res){
   res.type('text/plain');
   res.status(404);
   res.send('404 - Not found');
-})
+});
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
