d99e539626a8b35dc8bfe68ede5479d9c0e18a5e
Fortune <fortunefoluso26@gmail.com>
2025-07-06 22:18:10 +0100

    Using handlebars templating engine

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function(req, res){
-  res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello world');
-}).listen(3000);
+app.set('port', process.env.PORT || 3000);
+app.use(function(req, res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not found');
+})
 
-console.log(`Server is running at localhost:${port}`)
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Server Error');
+})
+
+app.listen(app.get('port'), function(){
+  console.log('Express is running on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate');
+});
